a4ca004c2e2f4b4fd839f93192cb16cfd7c2e4b7
TranLili <tranlili96@gmail.com>
2023-04-03 08:31:19 +0200

    Changed app_name. Added ToastClickListener class

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.webviewapp;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCute;
+    TextView viewCount;
+    ToastClickListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        btnCute = findViewById(R.id.btn_cute);
+        viewCount = findViewById(R.id.count);
+
+        listener = new ToastClickListener(viewCount);
+        btnCute.setOnClickListener(listener);
     }
 
 }
---
+++ app/src/main/java/com/example/webviewapp/ToastClickListener.java
@@ -0,0 +1,31 @@
+package com.example.webviewapp;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ToastClickListener implements View.OnClickListener {
+    private int count;
+    private TextView textView;
+
+    public ToastClickListener(TextView textView) {
+        this.textView = textView;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Context context = v.getContext();
+        CharSequence text = "You look cute!";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+        count++;
+        this.textView.setText("You are " + String.valueOf(count) + " x cuter than chocolate!");
+
+        System.out.println("Click!");
+    }
+}
